d7a9d9aafd48b522c2924cd839f3468c5d05543d
s1xp4c <blockstarterinfo@gmail.com>
2024-01-18 16:58:07 +0100

    Logos and general changes + Stripe

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,17 +1,17 @@
-'use client';
+"use client";
 
-import Button from '@/components/ui/Button';
-import { Database } from '@/types_db';
-import { postData } from '@/utils/helpers';
-import { getStripe } from '@/utils/stripe-client';
-import { Session, User } from '@supabase/supabase-js';
-import cn from 'classnames';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import Button from "@/components/ui/Button";
+import { Database } from "@/types_db";
+import { postData } from "@/utils/helpers";
+import { getStripe } from "@/utils/stripe-client";
+import { Session, User } from "@supabase/supabase-js";
+import cn from "classnames";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-type Subscription = Database['public']['Tables']['subscriptions']['Row'];
-type Product = Database['public']['Tables']['products']['Row'];
-type Price = Database['public']['Tables']['prices']['Row'];
+type Subscription = Database["public"]["Tables"]["subscriptions"]["Row"];
+type Product = Database["public"]["Tables"]["products"]["Row"];
+type Price = Database["public"]["Tables"]["prices"]["Row"];
 interface ProductWithPrices extends Product {
   prices: Price[];
 }
@@ -29,13 +29,13 @@ interface Props {
   subscription: SubscriptionWithProduct | null;
 }
 
-type BillingInterval = 'lifetime' | 'year' | 'month';
+type BillingInterval = "lifetime" | "year" | "month";
 
 export default function Pricing({
   session,
   user,
   products,
-  subscription
+  subscription,
 }: Props) {
   const intervals = Array.from(
     new Set(
@@ -46,21 +46,21 @@ export default function Pricing({
   );
   const router = useRouter();
   const [billingInterval, setBillingInterval] =
-    useState<BillingInterval>('month');
+    useState<BillingInterval>("month");
   const [priceIdLoading, setPriceIdLoading] = useState<string>();
 
   const handleCheckout = async (price: Price) => {
     setPriceIdLoading(price.id);
     if (!user) {
-      return router.push('/signin');
+      return router.push("/signin");
     }
     if (subscription) {
-      return router.push('/account');
+      return router.push("/account");
     }
     try {
       const { sessionId } = await postData({
-        url: '/api/create-checkout-session',
-        data: { price }
+        url: "/api/create-checkout-session",
+        data: { price },
       });
 
       const stripe = await getStripe();
@@ -78,7 +78,7 @@ export default function Pricing({
         <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
           <div className="sm:flex sm:flex-col sm:align-center"></div>
           <p className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            No subscription pricing plans found. Create them in your{' '}
+            No subscription pricing plans found. Create them in your{" "}
             <a
               className="text-pink-500 underline"
               href="https://dashboard.stripe.com/products"
@@ -100,12 +100,12 @@ export default function Pricing({
         <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
           <div className="sm:flex sm:flex-col sm:align-center">
             <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-              Pricing Plans
+              Frigear medlemskab
             </h1>
-            <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+            {/* <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
               Start building for free, then add a site plan to go live. Account
               plans unlock additional features.
-            </p>
+            </p> */}
             <div className="relative flex self-center mt-12 border rounded-lg bg-zinc-900 border-zinc-800">
               <div className="border border-pink-500 border-opacity-50 divide-y rounded-lg shadow-sm bg-zinc-900 divide-zinc-600">
                 <div className="p-6 py-2 m-1 text-2xl font-medium text-white rounded-md shadow-sm border-zinc-800 whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">
@@ -117,10 +117,10 @@ export default function Pricing({
               {products[0].prices?.map((price) => {
                 const priceString =
                   price.unit_amount &&
-                  new Intl.NumberFormat('en-US', {
-                    style: 'currency',
+                  new Intl.NumberFormat("en-US", {
+                    style: "currency",
                     currency: price.currency!,
-                    minimumFractionDigits: 0
+                    minimumFractionDigits: 0,
                   }).format(price.unit_amount / 100);
 
                 return (
@@ -148,8 +148,8 @@ export default function Pricing({
                       >
                         {products[0].name ===
                         subscription?.prices?.products?.name
-                          ? 'Manage'
-                          : 'Subscribe'}
+                          ? "Manage"
+                          : "Subscribe"}
                       </Button>
                     </div>
                   </div>
@@ -167,37 +167,37 @@ export default function Pricing({
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
         <div className="sm:flex sm:flex-col sm:align-center">
           <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            Pricing Plans
+            Frigear medlemskab
           </h1>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+          {/* <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
             Start building for free, then add a site plan to go live. Account
             plans unlock additional features.
-          </p>
+          </p> */}
           <div className="relative self-center mt-6 bg-zinc-900 rounded-lg p-0.5 flex sm:mt-8 border border-zinc-800">
-            {intervals.includes('month') && (
+            {intervals.includes("month") && (
               <button
-                onClick={() => setBillingInterval('month')}
+                onClick={() => setBillingInterval("month")}
                 type="button"
                 className={`${
-                  billingInterval === 'month'
-                    ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
-                    : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
+                  billingInterval === "month"
+                    ? "relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white"
+                    : "ml-0.5 relative w-1/2 border border-transparent text-zinc-400"
                 } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
               >
-                Monthly billing
+                Månedlig betaling
               </button>
             )}
-            {intervals.includes('year') && (
+            {intervals.includes("year") && (
               <button
-                onClick={() => setBillingInterval('year')}
+                onClick={() => setBillingInterval("year")}
                 type="button"
                 className={`${
-                  billingInterval === 'year'
-                    ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
-                    : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
+                  billingInterval === "year"
+                    ? "relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white"
+                    : "ml-0.5 relative w-1/2 border border-transparent text-zinc-400"
                 } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
               >
-                Yearly billing
+                Årlig betaling
               </button>
             )}
           </div>
@@ -208,20 +208,20 @@ export default function Pricing({
               (price) => price.interval === billingInterval
             );
             if (!price) return null;
-            const priceString = new Intl.NumberFormat('en-US', {
-              style: 'currency',
+            const priceString = new Intl.NumberFormat("en-US", {
+              style: "currency",
               currency: price.currency!,
-              minimumFractionDigits: 0
+              minimumFractionDigits: 0,
             }).format((price?.unit_amount || 0) / 100);
             return (
               <div
                 key={product.id}
                 className={cn(
-                  'rounded-lg shadow-sm divide-y divide-zinc-600 bg-zinc-900',
+                  "rounded-lg shadow-sm divide-y divide-zinc-600 bg-zinc-900",
                   {
-                    'border border-pink-500': subscription
+                    "border border-pink-500": subscription
                       ? product.name === subscription?.prices?.products?.name
-                      : product.name === 'Freelancer'
+                      : product.name === "Freelancer",
                   }
                 )}
               >
@@ -246,7 +246,7 @@ export default function Pricing({
                     onClick={() => handleCheckout(price)}
                     className="block w-full py-2 mt-8 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900"
                   >
-                    {subscription ? 'Manage' : 'Subscribe'}
+                    {subscription ? "Manage" : "Subscribe"}
                   </Button>
                 </div>
               </div>
@@ -263,55 +263,8 @@ function LogoCloud() {
   return (
     <div>
       <p className="mt-24 text-xs uppercase text-zinc-400 text-center font-bold tracking-[0.3em]">
-        Brought to you by
+        Her kommer awesome stuff!!
       </p>
-      <div className="flex flex-col items-center my-12 space-y-4 sm:mt-8 sm:space-y-0 md:mx-auto md:max-w-2xl sm:grid sm:gap-6 sm:grid-cols-5">
-        <div className="flex items-center justify-start">
-          <a href="https://nextjs.org" aria-label="Next.js Link">
-            <img
-              src="/nextjs.svg"
-              alt="Next.js Logo"
-              className="h-12 text-white"
-            />
-          </a>
-        </div>
-        <div className="flex items-center justify-start">
-          <a href="https://vercel.com" aria-label="Vercel.com Link">
-            <img
-              src="/vercel.svg"
-              alt="Vercel.com Logo"
-              className="h-6 text-white"
-            />
-          </a>
-        </div>
-        <div className="flex items-center justify-start">
-          <a href="https://stripe.com" aria-label="stripe.com Link">
-            <img
-              src="/stripe.svg"
-              alt="stripe.com Logo"
-              className="h-12 text-white"
-            />
-          </a>
-        </div>
-        <div className="flex items-center justify-start">
-          <a href="https://supabase.io" aria-label="supabase.io Link">
-            <img
-              src="/supabase.svg"
-              alt="supabase.io Logo"
-              className="h-10 text-white"
-            />
-          </a>
-        </div>
-        <div className="flex items-center justify-start">
-          <a href="https://github.com" aria-label="github.com Link">
-            <img
-              src="/github.svg"
-              alt="github.com Logo"
-              className="h-8 text-white"
-            />
-          </a>
-        </div>
-      </div>
     </div>
   );
 }
--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -1,7 +1,6 @@
-import Link from 'next/link';
-
-import Logo from '@/components/icons/Logo';
-import GitHub from '@/components/icons/GitHub';
+import GitHub from "@/components/icons/GitHub";
+import Logo from "@/components/icons/Logo";
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -15,7 +14,7 @@ export default function Footer() {
             <span className="mr-2 border rounded-full border-zinc-700">
               <Logo />
             </span>
-            <span>ACME</span>
+            <span>FRIGEAR</span>
           </Link>
         </div>
         <div className="col-span-1 lg:col-span-2">
@@ -25,7 +24,7 @@ export default function Footer() {
                 href="/"
                 className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
               >
-                Home
+                Hjem
               </Link>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
@@ -33,7 +32,7 @@ export default function Footer() {
                 href="/"
                 className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
               >
-                About
+                Info
               </Link>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
@@ -41,15 +40,7 @@ export default function Footer() {
                 href="/"
                 className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
               >
-                Careers
-              </Link>
-            </li>
-            <li className="py-3 md:py-0 md:pb-4">
-              <Link
-                href="/"
-                className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
-              >
-                Blog
+                Historie
               </Link>
             </li>
           </ul>
@@ -58,7 +49,7 @@ export default function Footer() {
           <ul className="flex flex-col flex-initial md:flex-1">
             <li className="py-3 md:py-0 md:pb-4">
               <p className="font-bold text-white transition duration-150 ease-in-out hover:text-zinc-200">
-                LEGAL
+                Jura
               </p>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
@@ -66,7 +57,7 @@ export default function Footer() {
                 href="/"
                 className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
               >
-                Privacy Policy
+                GDPR
               </Link>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
@@ -74,7 +65,7 @@ export default function Footer() {
                 href="/"
                 className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
               >
-                Terms of Use
+                Brugerpolitik
               </Link>
             </li>
           </ul>
@@ -83,7 +74,7 @@ export default function Footer() {
           <div className="flex items-center h-10 space-x-6">
             <a
               aria-label="Github Repository"
-              href="https://github.com/vercel/nextjs-subscription-payments"
+              href="https://github.com/s1xp4c/frigear-sub-payments"
             >
               <GitHub />
             </a>
@@ -92,19 +83,21 @@ export default function Footer() {
       </div>
       <div className="flex flex-col items-center justify-between py-12 space-y-4 md:flex-row bg-zinc-900">
         <div>
-          <span>
-            &copy; {new Date().getFullYear()} ACME, Inc. All rights reserved.
-          </span>
+          <span>&copy; {new Date().getFullYear()} Foreningen Frigear</span>
         </div>
         <div className="flex items-center">
-          <span className="text-white">Crafted by</span>
-          <a href="https://vercel.com" aria-label="Vercel.com Link">
+          <span className="text-white">App udviklet af</span>
+          <a
+            href="https://block-folio.netlify.app/"
+            aria-label="Portfolio link Link"
+          >
             <img
-              src="/vercel.svg"
-              alt="Vercel.com Logo"
-              className="inline-block h-6 ml-4 text-white"
+              src="/six_logo.png"
+              alt="Blockstarter Logo"
+              className="inline-block h-6 mx-3 text-white"
             />
           </a>
+          <span className="text-white">Morten Six</span>
         </div>
       </div>
     </footer>
