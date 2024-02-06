0ec85773bc5ae426ebc6dcaf1e06fc23b04fb709
s1xp4c <blockstarterinfo@gmail.com>
2024-02-06 03:47:06 +0100

    Phonenumber to string

--- emails/ContactEmail.tsx
+++ emails/ContactEmail.tsx
@@ -18,7 +18,7 @@ import * as React from "react";
 interface ContactEmailProps {
   name: string;
   emailAddress: string;
-  phoneNumber: number;
+  phoneNumber: string;
   subject: string;
   content: string;
 }
