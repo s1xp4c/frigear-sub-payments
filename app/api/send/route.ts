b79a01015e60ad866bef9c678c616c441f17de1f
s1xp4c <blockstarterinfo@gmail.com>
2024-02-06 04:19:07 +0100

    missing message from POST

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -28,7 +28,7 @@ export async function POST(req: Request) {
       to: [contactEmail],
       subject: subject,
       reply_to: emailAddress,
-      html: ContactEmail({
+      message: ContactEmail({
         name,
         emailAddress,
         subject,
