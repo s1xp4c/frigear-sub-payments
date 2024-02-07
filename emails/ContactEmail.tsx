1e677ba189c8a86c3111c9e10764076f58bd10d1
s1xp4c <blockstarterinfo@gmail.com>
2024-02-07 05:33:14 +0100

    Tailwind mail killer remove

--- emails/ContactEmail.tsx
+++ emails/ContactEmail.tsx
@@ -37,26 +37,19 @@ const ContactEmail = ({
     <Html>
       <Head />
       <Preview>{previewText}</Preview>
-      <Tailwind>
-        <Body style={main}>
-          <Container style={container}>
-            <Heading style={h1}>
-              Mail fra {name} med emne {subject}
-            </Heading>
-            <Img
-              src={`${baseUrl}/six-logo.png`}
-              width="33"
-              height="33"
-              alt="6"
-            />
-            <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
-            <Text style={{ ...text, marginBottom: "14px" }}>{content}</Text>
-            <Text style={footer}>
-              Sendt fra email {emailAddress} Fon: +45 {phoneNumber}
-            </Text>
-          </Container>
-        </Body>
-      </Tailwind>
+      <Body style={main}>
+        <Container style={container}>
+          <Heading style={h1}>
+            Mail fra {name} med emne {subject}
+          </Heading>
+          <Img src={`${baseUrl}/six-logo.png`} width="33" height="33" alt="6" />
+          <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
+          <Text style={{ ...text, marginBottom: "14px" }}>{content}</Text>
+          <Text style={footer}>
+            Sendt fra email {emailAddress} Fon: +45 {phoneNumber}
+          </Text>
+        </Container>
+      </Body>
     </Html>
   );
 };
