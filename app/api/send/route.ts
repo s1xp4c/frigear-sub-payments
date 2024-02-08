3f70d80e164c3e45a540316841e7d4db71e1a296
s1xp4c <blockstarterinfo@gmail.com>
2024-02-08 02:52:34 +0100

    Email path fix

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,6 +1,6 @@
 "use server";
 import { Resend } from "resend";
-import ContactEmail from "@/app/api/send/emails/ContactEmail";
+import ContactEmail from "./emails/ContactEmail";
 import * as z from "zod";
 import * as React from "react";
 
