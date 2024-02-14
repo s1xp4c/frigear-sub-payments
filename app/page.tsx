7c31422f2bbe90d062b185eb3e3e17c231c32b84
s1xp4c <blockstarterinfo@gmail.com>
2024-02-14 14:01:57 +0100

    Feature: added fb iframe news section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import EmailSection from "@/components/EmailSection";
 import HeroSection from "@/components/HeroSection";
+import News from "@/components/News/page";
 import CustomToast from "@/components/Toast/Toast";
 import React from "react";
 
@@ -10,6 +11,7 @@ export default function home() {
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-6">
         <div className="sm:flex sm:flex-col sm:align-center">
           <HeroSection></HeroSection>
+          <News />
           <EmailSection></EmailSection>
         </div>
       </div>
