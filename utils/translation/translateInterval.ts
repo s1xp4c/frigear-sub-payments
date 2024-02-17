82d8ca9a9285315dce14835d6ccc817e2fa3288b
s1xp4c <blockstarterinfo@gmail.com>
2024-02-17 08:46:40 +0100

    Bugfix naming on buttons

--- utils/translation/translateInterval.ts
+++ utils/translation/translateInterval.ts
@@ -2,11 +2,11 @@
 // Utils function to get the billing interval as a string
 export const translateInterval = (interval: string, count: number) => {
     switch (interval) {
-      case 'year':
+      case 'month':
         if (count === 1) return 'ÅRLIG';
         if (count === 3) return 'KVARTAL';
         break;
-      case 'month':
+      case 'year':
         return 'MÅNED';
       default:
         return 'SUPPORT';
