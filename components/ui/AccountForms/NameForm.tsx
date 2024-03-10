1eaf94906d4fd58e413908ac75e994cf7c6dcbe6
s1xp4c <blockstarterinfo@gmail.com>
2024-03-10 14:37:23 +0100

    More NameForm update

--- components/ui/AccountForms/NameForm.tsx
+++ components/ui/AccountForms/NameForm.tsx
@@ -6,11 +6,12 @@ import { updateName } from "@/utils/auth-helpers/server";
 import { handleRequest } from "@/utils/auth-helpers/client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+// import { string } from "zod";
 
 export default function NameForm({ userName }: { userName: string }) {
   const router = useRouter();
   const [isSubmitting, setIsSubmitting] = useState(false);
-
+  const firstName = userName.split(" ")[0];
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     setIsSubmitting(true);
     // Check if the new name is the same as the old name
@@ -25,8 +26,8 @@ export default function NameForm({ userName }: { userName: string }) {
 
   return (
     <Card
-      title={`"Fuldt navn: " ${userName}`}
-      description="Her er dit smækre fulde navn. Du kan opdatere det hvis du vil, men vi bruger det kun til at finde dig i vrimlen og det vises ikke offenligt."
+      title={`Fuldt navn`}
+      description={`Her er dit smækre fulde navn ${firstName}. Du kan opdatere det hvis du vil, men vi bruger det kun til at finde dig i vrimlen og det vises ikke offenligt.`}
       footer={
         <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
           <p className="pb-4 sm:pb-0">Max 64 karakterer</p>
@@ -48,7 +49,7 @@ export default function NameForm({ userName }: { userName: string }) {
             name="fullName"
             className="sm:w-1/2 p-3 rounded-md bg-zinc-800 w-full"
             defaultValue={userName}
-            placeholder="Dit skønne fulde navn..."
+            placeholder={userName ? userName : `Dit skønne fulde navn...`}
             maxLength={64}
           />
         </form>
